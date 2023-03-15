8ffde776597d8700ff8d8a418b08970636532665
chrisjchandler <chrischandler@live.com>
2023-03-15 19:50:12 +0000

    second commit

--- chatbot.py
+++ chatbot.py
@@ -12,13 +12,13 @@ def index():
 @app.route("/chat", methods=["POST"])
 def chat():
     user_input = request.json["user_input"]
-    conversation_history = request.json["conversation_history"]
+    conversation_history = request.json["user_input"]
     prompt = f"{conversation_history}User: {user_input}\nAI:"
 
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
-        max_tokens=3750,
+        max_tokens=3700,
         n=1,
         stop=None,
         temperature=0.8,
